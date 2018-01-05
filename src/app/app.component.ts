ee9e8669c8f51528b811f785bcdedac410508341
Fabrizio Ricciarelli <fabrizioricciarelli@gmail.com>
2018-01-05 16:44:02 +0100

    Esempi di formattazione aggiunti

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 
 // todo
 import { Model, TodoItem } from './models/model';
+import { NgSwitch } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['bootstrap.css', './app.component.css']
 })
 export class AppComponent {
   title = 'tests';
-
+  public frameworks: string[] = ['Angular', 'React', 'Ember'];
+  public selectedcar = 'Ferrari';
   // todo
   model = new Model();
   getName() {
@@ -23,6 +25,14 @@ export class AppComponent {
     if (newItem !== '') {
       this.model.items.push(new TodoItem(newItem, false));
     }
+  }
+  getInlineStyles(framework) {
+    const styles = { 'color': framework === 'Angular' ? 'red' : 'green', 'text-decoration': framework === 'Angular' ? 'underline' : 'none'};
+    return styles;
+  }
+  geClasses(framework) {
+    const classes = { red: framework = 'Angular', bolder: framework = 'Angular' };
+    return classes;
   }
 }
 
