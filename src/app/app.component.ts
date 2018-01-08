47af7c816fedd121adc37eeaa346223d0ef6d072
Fabrizio Ricciarelli <fabrizioricciarelli@gmail.com>
2018-01-08 13:18:08 +0100

    Sistemato pulsante "Add" con svuotamento del campo di input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 // todo
 import { Model, TodoItem } from './models/model';
 import { Book } from './models/book';
 import { BOOKS } from './models/mock-book';
 import { NgSwitch } from '@angular/common';
+import { FormControl } from '@angular/forms/src/model';
 
 @Component({
   selector: 'app-root',
@@ -19,36 +20,9 @@ export class AppComponent implements OnInit {
     { name: 'React', url: 'assets/img/reactLogo.png' },
     { name: 'Ember', url: 'assets/img/emberLogo.jpg' }
   ];
-/*   public BOOKS: Book[] = [
-    {
-      isbn: 9781786462084,
-      title: 'Laravel 5.x Cookbook',
-      authors: 'Alfred Nutile',
-      published: 'September 2016',
-      description: 'A recipe-based book to help you efficiently create amazing PHP-based applications with Laravel 5.x',
-      coverImage: 'https://d255esdrn735hr.cloudfront.net/sites/default/files/imagecache/ppv4_main_book_cover/B05517_MockupCover_Cookbook_0.jpg'
-    },
-    {
-      isbn: 9781784396527,
-      title: 'Sitecore Cookbook for Developers',
-      authors: 'Yogesh Patel',
-      published: 'April 2016',
-      description: 'Over 70 incredibly effective and practical recipes to get you up and running with Sitecore development',
-      coverImage: 'https://d255esdrn735hr.cloudfront.net/sites/default/files/imagecache/ppv4_main_book_cover/6527cov_.jpg'
-    },
-    {
-      isbn: 9781783286935,
-      title: 'Sass and Compass Designers Cookbook',
-      authors: 'Bass Jobsen',
-      published: 'April 2016',
-      description: 'Over 120 practical and easy-to-understand recipes that explain how to use Sass and Compass to write efficient, maintainable, and reusable CSS code for your web development projects',
-      coverImage: 'https://d1ldz4te4covpm.cloudfront.net/sites/default/files/imagecache/ppv4_main_book_cover/I6935.jpg'
-    }
-  ];
- */  
   public model: Model;
-  public booksList: Book[];
-
+  public booksList: Book[] = BOOKS;
+  @ViewChild('todoText') el: ElementRef;
   public selectedcar = 'Ferrari';
   public message: string;
   public colSpanValue = 3;
@@ -56,11 +30,9 @@ export class AppComponent implements OnInit {
   constructor() {
     this.title = 'tests';
     this.model = new Model();
-    this.booksList = new Array<Book>();
   }
 
   ngOnInit(): void {
-    this.booksList = BOOKS;
   }
 
   // todo
@@ -73,6 +45,7 @@ export class AppComponent implements OnInit {
   addItem(newItem) {
     if (newItem !== '') {
       this.model.items.push(new TodoItem(newItem, false));
+      this.el.nativeElement.value = '';
     }
   }
   getInlineStyles(framework) {
@@ -88,6 +61,9 @@ export class AppComponent implements OnInit {
   }
   showMessage(msg) {
     this.message = msg.target.value;
+  }
+  getProductClasses(availability: number | null): string {
+    return (availability === null || this.booksList.length >= availability) ? 'bg-success' : 'bg-warning';
   }
   getBook(isbn: number) {
     const consoleMsg = `First book: ${this.booksList[0].title}`;
